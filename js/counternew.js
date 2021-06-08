c391256f1963f2c4986ba25b2f30133cdcdfeae6
Aryan Nagar <40473422+aryannagar@users.noreply.github.com>
2021-06-08 12:26:52 +0530

    Add files via upload

---
+++ js/counternew.js
@@ -0,0 +1,30 @@
+<script type="text/javascript">
+            $(document).ready(function(){
+                $('#count-box').CountUpCircle({
+                    duration: 2000,
+                    opacity_anim: false,
+                    step_divider: 1
+                });
+            });
+        </script>
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('#count-box2').CountUpCircle({
+                    duration: 2000,
+                    opacity_anim: false,
+                    step_divider: 1
+                });
+            });
+        </script>
+
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('#count-box3').CountUpCircle({
+                    duration: 800,
+                    opacity_anim: false,
+                    step_divider:  1,
+                });
+            });
+        </script>
